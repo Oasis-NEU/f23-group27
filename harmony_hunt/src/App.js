c59802863981a49ad78a0ac495780195c1a48baf
henryrenninger <henryrenninger@gmail.com>
2023-10-22 13:40:12 -0400

    full front-end touch-ups

--- harmony_hunt/src/App.js
+++ harmony_hunt/src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <img src={logo} alt="Logo" className="App-logo" />
       <header className="App-header">
-        <img src={logo} alt="Logo" className="App-logo" />
         <h1>Harmony Hunter</h1>
         <h4>BE IN TUNE WITH YOUR TUNES</h4>
         <button><img src={connect_with_spotify} className="Connect-with-spotify" width='250' height='50'/></button>
