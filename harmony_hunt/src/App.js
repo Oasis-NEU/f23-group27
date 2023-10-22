f6fbbfecd37032f40f0ac457827325a856891b4a
henryrenninger <henryrenninger@gmail.com>
2023-10-22 12:59:18 -0400

    Fixed a styling issue with the magnifying glass icon

--- harmony_hunt/src/App.js
+++ harmony_hunt/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+import logo from './spotify-2.svg';
+import mg from './magnifying-glass.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} alt="Logo" />
+        <img src={logo} alt="Logo" className="App-logo" />
+        <img src={mg} className="Magnifying-glass" />
         <h1>Harmony Hunter</h1>
         <button>Connect to Spotify</button>
       </header>
