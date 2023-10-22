57c04c61cd261d341d1d93e4268fc58ce9364cab
henryrenninger <henryrenninger@gmail.com>
2023-10-22 12:32:55 -0400

    front-end

--- harmony_hunt/src/App.js
+++ harmony_hunt/src/App.js
@@ -5,18 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} alt="Logo" />
+        <h1>Harmony Hunter</h1>
+        <button>Connect to Spotify</button>
       </header>
     </div>
   );
