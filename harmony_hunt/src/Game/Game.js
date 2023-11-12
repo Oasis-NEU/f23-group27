fd0e5dbcd574de6ec96a54c7840ca91d8e7491a5
Shoumik Majumdar <shoumik123majumdar@gmail.com>
2023-11-12 12:48:01 -0500

    Rebased Danny's branch manually by adding the files to the Game directory

---
+++ harmony_hunt/src/Game/Game.js
@@ -0,0 +1,40 @@
+import React, { useState, useRef } from 'react';
+import './harmony-hunt-logo.png';
+import './App.css';
+import './style.css'
+import Header from './Header'
+import Hints from './Hints';
+function App() { 
+    
+  const guessRef = useRef();
+  const test_song = {
+    title:"My Name is Mud",
+    genre:"Alternative",
+    artist:"Primus",
+    year:"1993"
+  }
+  
+  function handleGuess(e) {
+    const guess = guessRef.current.value;
+    if (guess.toUpperCase() === test_song.title.toUpperCase()) {
+      
+    }
+    
+  }
+  
+  return (
+    <>
+    <Header />
+    <div id="all-boxes">
+      <div id="guess-box"  className="hint">
+        <input ref={guessRef}type="text" id="guess-input" placeholder="Enter Guess..."></input>
+        <button id="guess-button" onClick={handleGuess}></button>
+      </div>
+      <Hints />
+    </div>
+    <p>{  }</p>
+    </>
+    
+  );
+}
+export default App;
