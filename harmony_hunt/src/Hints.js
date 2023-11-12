b08d231466afd56ed8c7856b092adbc867876729
dansheehan5 <danielsheehan527@gmail.com>
2023-11-12 12:54:53 -0500

    implemented game functionality

--- harmony_hunt/src/Hints.js
+++ harmony_hunt/src/Hints.js
@@ -1,24 +1,24 @@
 import { useState, useRef } from 'react';
 
-export default function Hints() {
+export default function Hints({ hint }) {
     return(
         <div id="hint-and-guess-boxes">
-            <div id="album-cover"></div>
+            <div id="album-cover">{hint.album}</div>
             <div id="hint-boxes">
             <div className="hint">
-                <p id="genre" className="hint-text">Genre:</p>
+                <p id="genre" className="hint-text">{hint.genre}</p>
             </div>
             <div className="hint">
-                <p id="artist" className="hint-text">Artist:</p>
+                <p id="year" className="hint-text">{hint.year}</p>
             </div>
             <div className="hint">
-                <p id="year" className="hint-text">Year Released:</p>
+                <p id="artist" className="hint-text">{hint.artist}</p>
             </div>
             <div id="snippet" className="hint">
                 <button id="snippet-button">
                     <div id="play-button"></div>
                 </button>
-                <p id="snippet-text" class="hint-text">Play Snippet</p>
+                <p id="snippet-text" class="hint-text">{hint.snippet}</p>
             </div>
             </div>
         </div>
--- harmony_hunt/src/game.js
+++ harmony_hunt/src/game.js
@@ -9,38 +9,62 @@ function App() {
     
 
   const guessRef = useRef();
+  
+  const[gameState, setGameState] = useState(1);
+  const [game, setGame] = useState({genre:"Genre", year:"Year", artist:"Artist", album:"Album", snippet:"Play Snippet"});
+
   const test_song = {
     title:"My Name is Mud",
     genre:"Alternative",
+    year:"1993",
     artist:"Primus",
-    year:"1993"
+    album:"Pork Soda",
+    snippet:"snippet played"
   }
-  
-
 
   function handleGuess(e) {
     const guess = guessRef.current.value;
-    if (guess.toUpperCase() === test_song.title.toUpperCase()) {
-      
+    setGameState(gameState + 1);
+    if (gameState != 6) {
+      if (guess.toUpperCase() === test_song.title.toUpperCase()) {
+        setGameState(7);
+        setGame({genre:test_song.genre, year:test_song.year, artist:test_song.artist, album:test_song.album, snippet:test_song.snippet});
+      } else {
+        switch (gameState) {
+          case 1:
+            setGame({genre:test_song.genre, year:game.year, artist: game.artist, album: game.album, snippet: game.snippet});
+            break;
+          case 2:
+            setGame({genre:game.genre, year:test_song.year, artist: game.artist, album: game.album, snippet: game.snippet});
+            break;
+          case 3:
+            setGame({genre:game.genre, year:game.year, artist: test_song.artist, album: game.album, snippet: game.snippet})
+            break;
+          case 4:
+            setGame({genre:game.genre, year:game.year, artist: game.artist, album: test_song.album, snippet: game.snippet});
+            break;
+          case 5:
+            setGame({genre:game.genre, year:game.year, artist: game.artist, album: game.album, snippet: test_song.snippet});
+            break;
+          case 6:
+            setGame({genre:test_song.genre, year:test_song.year, artist:test_song.artist, album:test_song.album, snippet:test_song.snippet});
+        }
+      }
     }
-
-    
-
+    guessRef.current.value = null;
   }
   
   return (
     <>
-    <Header />
+    <Header gameMode={ gameState }/>
     <div id="all-boxes">
       <div id="guess-box"  className="hint">
         <input ref={guessRef}type="text" id="guess-input" placeholder="Enter Guess..."></input>
-        <button id="guess-button" onClick={handleGuess}></button>
+        <button id="guess-button" onClick={handleGuess} disabled={(gameState === 6)||(gameState === 7)}></button>
       </div>
-      <Hints />
+      <Hints hint={{ genre:game.genre, year:game.year, artist:game.artist, album:game.album, snippet:game.snippet}}/>
     </div>
-    <p>{  }</p>
     </>
-    
   );
 }
 
